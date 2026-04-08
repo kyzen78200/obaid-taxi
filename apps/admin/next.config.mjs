b98de2fc5222e86ceca0fc4c3cc22f99ce85d403
ZeBreyn <zebreyn@gmail.com>
2026-04-08 16:19:50 +0200

    config: update production URLs for admin and mobile

--- apps/admin/next.config.mjs
+++ apps/admin/next.config.mjs
@@ -2,7 +2,11 @@
 const nextConfig = {
   experimental: {
     serverActions: {
-      allowedOrigins: ['localhost:3001'],
+      allowedOrigins: [
+        'localhost:3001',
+        'obaid-taxi-admin.vercel.app',
+        'obaid-taxi-admin-h452p7r3b-zebreyn-9679s-projects.vercel.app',
+      ],
     },
   },
 }
