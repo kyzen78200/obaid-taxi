5e08ce7890242eb3bc0345fa999160df5458bb4f
ZeBreyn <zebreyn@gmail.com>
2026-04-18 10:00:03 +0200

    chore: bump version to 1.0.1 + remote appVersionSource

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -29,7 +29,7 @@ module.exports = {
   expo: {
     name: 'O Taxi',
     slug: 'obaid-taxi',
-    version: '1.0.0',
+    version: '1.0.1',
     orientation: 'portrait',
     icon: './assets/icon.png',
     userInterfaceStyle: 'light',
